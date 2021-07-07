030f9ba10a3ef1f7a76e0a8678c792c638d48346
Parthasarathy R <98parthar@gmail.com>
2021-07-07 21:37:09 +0530

    Commiting changes to Number Puzzle Program

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -35,19 +35,52 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		int emptyCellId = game.getEmptyCellId();
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
-		
-		//Your logic here		
-		
+		//Your logic here
+		for(int i=0; i<=15;i++)
+		{
+			if (buttonClicked == buttons[i])
+			{
+				System.out.print("Hello"+ emptyCellId + i);
+				if(
+						(
+								(java. lang. Math. abs((i/4)-(emptyCellId/4))==1) && 
+								(java. lang. Math. abs((i%4)-(emptyCellId%4))==0)
+						)
+						||
+						(
+								(java. lang. Math. abs((i/4) - (emptyCellId/4)) == 0) && 
+								
+								(java. lang. Math. abs((i%4)- (emptyCellId%4)) == 1))
+						)
+				{
+					swapButton(buttons[emptyCellId], buttonClicked);
+					emptyCellId = i;
+					break;
+				}
+
+			}
+		}
 		return emptyCellId;
 
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
-		
 		//Your logic here
+		java.util.Set<Integer> set = new java.util.HashSet<Integer>();
 		int a = getRandomNumber();
-		
-		
+		set.add(a%15 + 1);
+		arr[0] = a%15+1;
+		for(int i=1;i<15; i++)
+		{
+			a = getRandomNumber();
+			while(set.contains(a%15+1))
+			{
+				a = getRandomNumber();
+				
+			}
+			set.add(a%15+1);
+			arr[i] = a%15+1;			
+		}
 		return arr;
 	}
 	public boolean checkForWinner(Button[] buttons)
@@ -55,8 +88,15 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
+		 int [] NumArray = getIntegerArrayOfButtonIds(buttons);
+		 for (int i=0; i<14; i++)
+		 {
+			if (NumArray[i] > NumArray[i+1])
+			{
+				winner = false;
+				break;
+			}
+		 }
 		return winner;
 	}
 }
